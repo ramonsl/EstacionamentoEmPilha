861e1d8b0f5a34f436a5c5c2f31390982354627e
ramonsl <ramonsl@gmail..com>
2018-04-10 19:52:59 -0300

    Final
    Exercicio final

--- estacionamento.js
+++ estacionamento.js
@@ -21,6 +21,36 @@ class Vagas{
     getSize(){
         return this.topo+1;
     }
+
+    push(carro){
+        if(this.isFull()){
+            console.log("Impossivel adicionar")
+            return false;
+        }else{
+            this.topo++;
+            this.vetor[this.topo]=carro;
+            return true;
+        }
+    }
+    pop(){
+        if(this.isEmpty()){
+            console.log("Estamos vazio! Galera usa UBER!");
+        }else{
+            var c = this.vetor[this.topo];
+            this.topo--;
+            return c;
+        }
+    }
+    listar(){
+        if(this.isEmpty()){
+            console.log("Estamos vazio! Galera usa UBER!");
+        }else{
+            for(var i=0;i<=this.topo;i++){
+                this.vetor[i].mostrar();
+            }
+        }
+    }
+
 }
 class Carro{
     constructor(p){
@@ -31,8 +61,8 @@ class Carro{
         this.manobras++;
     }
     mostrar(){
-        console.log("Veiculo : "+this.placa);
-        console.log("Efetuou : "+this.manobras);
+        console.log("Veiculo : "+this.placa+ " manobras : "+this.manobras);
+      
     }
 
 
@@ -42,21 +72,65 @@ class Carro{
 
 var estacionamento = new Vagas();
 var rua = new Vagas();
-console.log("Digite 1 para adicionar o veiculo");
-console.log("Digite 2 para remover o veiculo");
 op=0;
 do{
-    op=parseInt(prompt("Digite 1 para adicionar o veiculo\n Digite 2 para remover o veiculo\n 99- Sair"));
+    op=parseInt(prompt("Digite 1 para adicionar o veiculo\
+     \nDigite 2 para remover o veiculo\
+     \nDigite 3 para ver os veiculos no estacionamento\
+     \nDigite 4 para ver os veiculos no rua\
+     \n 99- Sair"));
 
 
     switch(op){
         case 1:{
             var a = new Carro(prompt("Qual a placa do veiculo?"));
+            if(estacionamento.push(a)){
+                a.mostrar();
+                console.log("Adicionando com sucesso!")
+            }else{
+                console.log("Lotado!")
+            }
+           
+            
             break;
         }
 
         case 2:{
             var a = new Carro(prompt("Qual a placa do veiculo?"));
+            
+            do{
+                var rem= estacionamento.pop();
+                if(rem.placa===a.placa){
+                    rem.mostrar();
+                    console.log("Veiculo deixando o estacionamento ");
+                    rem.efetuaManobra();
+                }else{
+                    console.log("Veiculo movido para a rua "+rem.placa);
+                    rem.efetuaManobra();
+                    rua.push(rem);
+                }
+            }while(rem.placa!=a.placa)
+       
+            for(var i=0;i<=rua.topo+1;i++){
+            
+                add=rua.pop();
+                console.log("Veiculo movido para a estacionamento "+add.placa);
+                add.efetuaManobra();
+                estacionamento.push(add);
+            }
+
+
+
+            break;
+        }
+        case 3:{
+            console.log("Veiculos no estacionamento:")
+            estacionamento.listar();
+            break;
+        }
+        case 4:{
+            console.log("Veiculos na rua:")
+            rua.listar();
             break;
         }
         case 99:{
